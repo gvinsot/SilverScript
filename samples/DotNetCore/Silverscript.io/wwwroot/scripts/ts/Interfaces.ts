6d4be543017e1804ed836c5528b3f8150cdc5757
givinsot <givinsot@DESKTOP-PVT32I7>
2017-07-05 00:03:18 +0200

    missing ts files

---
+++ samples/DotNetCore/Silverscript.io/wwwroot/scripts/ts/Interfaces.ts
@@ -0,0 +1,13 @@
+﻿///<reference path="EventHandler.ts" />
+
+module SS {
+    // Interface
+    export interface IDisposable {
+        Dispose(): void;
+    }
+
+    export interface INotifyPropertyChanged {
+        PropertyChanged: EventHandler;
+    }
+
+}
---
+++ samples/DotNetCore/Silverscript.io/wwwroot/scripts/ts/StopWatch.ts
@@ -0,0 +1,16 @@
+﻿module SS {
+
+    export class StopWatch {
+        public _startTime: number;
+        // Constructor
+        constructor() {
+            this._startTime = Date.now();
+        }
+        public get StartTime(): number {
+            return this._startTime;
+        }
+        public get Elapsed(): number {
+            return Date.now() - this._startTime;
+        }
+    }
+}
